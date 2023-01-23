d9b1033ff1d43562fa5326544671898a16213329
Isaas3 <90279369+Isaas3@users.noreply.github.com>
2023-01-23 14:35:21 -0600

    Second commit
    Meteors and lasers destroy off screen

--- main.py
+++ main.py
@@ -1,41 +1,109 @@
 import pygame, sys
+from random import randint, uniform
 
 class Ship(pygame.sprite.Sprite):
     def __init__(self, groups):
-        super().__init__(groups) 
-        self.image = pygame.image.load("./graphics/ship.png").convert_alpha()
-        self.rect = self.image.get_rect(center = (WINDOW_WIDTH /2, WINDOW_HEIGHT /2))
+        super().__init__(groups) # 1. We have to init the parent class
+        self.image = pygame.image.load("./graphics/ship.png").convert_alpha() # 2. We need a surface -> image
+        self.rect = self.image.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)) # 3. We need a rect
 
         # timer
         self.can_shoot = True
         self.shoot_time = None
-        
+    
     def laser_timer(self):
         if not self.can_shoot:
             current_time = pygame.time.get_ticks()
-            if current_time - self.shoot_time > 500:
+            if current_time - self.shoot_time > 200:
                 self.can_shoot = True
-
+    
     def input_position(self):
         pos = pygame.mouse.get_pos()
         self.rect.center = pos
 
     def laser_shoot(self):
         if pygame.mouse.get_pressed()[0] and self.can_shoot:
-            print('shoot laser')
             self.can_shoot = False
             self.shoot_time = pygame.time.get_ticks()
+
+            Laser(self.rect.midtop,laser_group)
+
+    def meteor_collision(self):
+        if pygame.sprite.spritecollide(self,meteor_group,False):
+            pygame.quit()
+            sys.exit()
 
     def update(self):
         self.input_position()
         self.laser_shoot()
         self.laser_timer()
+        self.meteor_collision()
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load("./graphics/laser.png").convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
+
+        # float based position
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(0,-1)
+        self.speed = 600
+    
+    def meteor_collision(self):
+       if pygame.sprite.spritecollide(self,meteor_group,True):
+            self.kill()
+
+    def update(self):
+        self.pos += self.direction * self.speed * dt
+        self.rect.topleft = (round(self.pos.x),round(self.pos.y))
+
+        if self.rect.bottom < 0:
+            self.kill()
+
+        self.meteor_collision()
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self,pos,groups):
+
+        # basic setup
+        super().__init__(groups)
+        self.image = pygame.image.load('./graphics/meteor.png').convert_alpha()
+        self.rect = self.image.get_rect(center = pos)
+
+        # float based positioning
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        self.direction = pygame.math.Vector2(uniform(-0.5, 0.5),1)
+        self.speed = randint(400,600)
+    
+    def update(self):
+        self.pos += self.direction * self.speed * dt
+        self.rect.topleft = (round(self.pos.x),round(self.pos.y))
+        if self.rect.top > WINDOW_HEIGHT:
+            self.kill()
+
+class Score:
+    def __init__(self):
+        self.font = pygame.font.Font('./graphics/subatomic.ttf',50)
+    
+    def display(self):
+        score_text = f'Score: {pygame.time.get_ticks() // 1000}'
+        text_surf = self.font.render(score_text, True, (255,255,255))
+        text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
+        display_surface.blit(text_surf, text_rect)
+        pygame.draw.rect(
+            display_surface, 
+            (255,255,255), 
+            text_rect.inflate(30, 30), 
+            width = 8, 
+            border_radius = 5
+        )
+
+# exercise
+# 1. create a timer
+# 2. when the timer triggers, create a meteor sprite
+# 3. you need t ocreate a meteor sprite class like the laser + meteor sprite group
+# 4. the meteor movement should be a bit more random and the start pos is at the top of the window
 
 # basic setup
 pygame.init()
@@ -50,34 +118,50 @@ background_surf = pygame.image.load('./graphics/background.png').convert()
 # sprite groups
 spaceship_group = pygame.sprite.GroupSingle()
 laser_group = pygame.sprite.Group()
+meteor_group = pygame.sprite.Group()
 
 # sprite creation
 ship = Ship(spaceship_group)
-laser = Laser((1000,300), laser_group)
+# laser = Laser((100,300), laser_group)
+
+# timer
+meteor_timer = pygame.event.custom_type()
+pygame.time.set_timer(meteor_timer,400)
+
+# score
+score = Score()
 
 # main game loop
 while True:
 
-    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        if event.type == meteor_timer:
+            meteor_y_pos = randint(-150, -50)
+            meteor_x_pos = randint(-100, WINDOW_WIDTH + 100)
+            Meteor((meteor_x_pos, meteor_y_pos), groups = meteor_group)
+    
     # delta time
-    dt = clock.tick() / 1000
+    dt = clock.tick(60) / 1000
+    # print(clock.get_fps())
 
     # background
     display_surface.blit(background_surf,(0,0))
 
     # update
     spaceship_group.update()
+    laser_group.update()
+    meteor_group.update()
+
+    # score display
+    score.display()
 
     # graphics
     spaceship_group.draw(display_surface)
     laser_group.draw(display_surface)
+    meteor_group.draw(display_surface)
 
     # draw the frame
     pygame.display.update()
-
-
